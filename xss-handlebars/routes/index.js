fdb4eb7d8097c106111d442210b1f65ffd6e31dc
Alexander Reinthal <alexander.reinthal@gmail.com>
2018-05-21 00:03:28 +0200

    [task] initial commit

---
+++ xss-handlebars/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+// How to add parmaeters to post routes
+router.post('/input', function(req, res, next){
+    var item = {
+        type: req.body.type,
+        content: req.body.content,
+        script: false,
+        html: false,
+        href: false,
+        css: false
+    };
+    if(item.type == "script") {
+      item.script = true;
+    } else if(item.type == "html") {
+      item.html = true;
+    } else if (item.type == "href") {
+      item.href = true;
+    } else if (item.type == "css") {
+      item.css = true;
+    }
+    res.render('index', item);
+});
+
+
+
+// How to add parameters to get routes
+router.get('/test/:id', function(req, res, next){
+  res.render('test', {output: req.params.id});
+});
+
+
+
+
+module.exports = router;
